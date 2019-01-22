18d8cee49f83c321583927f5abf97a862cd303ce
Hal Helms <hal.helms@gmail.com>
2019-01-22 02:38:53 -0800

    finished 6 exercises

---
+++ index.js
@@ -0,0 +1,37 @@
+// Arrays Labs
+// Rev. 22Jan2019
+
+/*
+1. Create an array, singers, with the following singers
+Beyoncé,
+Mariah Carey,
+Drake,
+Bruno Mars,
+Adele,
+Adam Levine,
+Rihanna,
+Lady Gaga,
+Kanye,
+Christina Aguilera,
+Billy Joel
+*/
+
+/*
+2. Using the forEach function, loop over the singers array, outputting each name to the console
+*/
+
+/*
+3. Output the 5th singer in the group
+*/
+
+/*
+4. Add a singer, Alicia Keys, just before Bruno Mars
+*/
+
+/*
+5. Add a singer, Ed Sheeran, to the end of the singers array
+*/
+
+/*
+6. Remove Billy Joel from the singers array
+*/
